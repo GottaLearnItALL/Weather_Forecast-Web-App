1f1030b78a5ccd0ce97f69703ac7707289fb981e
Aryan Bhat <aryanbhat.2017@gmail.com>
2024-08-09 20:28:51 +0530

    filter the data

--- backend.py
+++ backend.py
@@ -7,8 +7,15 @@ def get_data(place, forecast_days=None, type_=None):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
-    return data
+    filtered_data = data["list"]
+    nr_values = 8 * forecast_days
+    filtered_data = filtered_data[:nr_values]
+    if type_ == "Temperature":
+        filtered_data = [dict_["main"]["temp"] for dict_ in filtered_data]
+    elif type_ == "Sky":
+        filtered_data = [dict["weather"]["main"] for dict in filtered_data]
+    return filtered_data
 
 
 if __name__ == "__main__":
-    print(get_data(place="tokyo"))
+    print(get_data(place="tokyo",forecast_days=3, type_="temperature"))
